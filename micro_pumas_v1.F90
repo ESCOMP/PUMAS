a60ec55cbd63d746f62cc29925f47e66badf56f4
Cheryl Craig <cacraig@ucar.edu>
2023-01-26 09:37:16 -0700

    Fix merge bug and move stochastic_collect_tau_cam.F90 to CAM src

--- micro_pumas_v1.F90
+++ micro_pumas_v1.F90
@@ -2221,7 +2221,9 @@ subroutine micro_pumas_tend ( &
 
   call heterogeneous_rain_freezing(t, qric, nric, lamr, mnuccr, nnuccr, mgncol*nlev)
 
-  if (trim(warm_rain) == 'sb2001' .or. trim(warm_rain) == 'kk2000') then
+  if (trim(warm_rain) == 'sb2001') then
+     call sb2001v2_accre_cld_water_rain(qcic, ncic, qric, rho, relvar, pra, npra, mgncol*nlev)
+  else
      !$acc parallel vector_length(VLENS) default(present)
      !$acc loop gang vector collapse(2)
      do k = 1,nlev
@@ -2240,11 +2242,7 @@ subroutine micro_pumas_tend ( &
      end do
      !$acc end parallel
 
-     if (trim(warm_rain) == 'sb2001') then
-        call sb2001v2_accre_cld_water_rain(ctmp, ntmp, rtmp, rho, relvar, pra, npra, mgncol*nlev)
-     else
-        call accrete_cloud_water_rain(microp_uniform, rtmp, ctmp, ntmp, relvar, accre_enhan, pra, npra, mgncol*nlev)
-     endif
+     call accrete_cloud_water_rain(microp_uniform, rtmp, ctmp, ntmp, relvar, accre_enhan, pra, npra, mgncol*nlev)
   endif
 
   !$acc parallel vector_length(VLENS) default(present)
